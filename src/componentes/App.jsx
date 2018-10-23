96b12a976501f3ef3b5668d9c273e5a7dfde2f77
darioulin <dariomuse95@gmail.com>
2018-10-23 14:41:03 -0500

    Formulario agregado

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import Header from "./Header.jsx";
+import Formulario from "./Formulario.jsx";
 class App extends Component {
   render() {
     return (
       <div className="contenedor">
         <Header titulo="Cotizador de seguro de auto" />
+        <div className="contenedor-formulario">
+          <Formulario />
+        </div>
       </div>
     );
   }
