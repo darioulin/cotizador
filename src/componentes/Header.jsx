8f6004c4557fff55018ffd24cddf951ae51a8ef1
darioulin <dariomuse95@gmail.com>
2018-10-23 14:24:37 -0500

    Header Added

---
+++ src/componentes/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = props => {
+  return (
+    <header className="top">
+      <h1>Cotizador de Seguro de Auto</h1>
+    </header>
+  );
+};
+
+export default Header;
