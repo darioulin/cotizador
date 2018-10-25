354858a2701f8afead5ac62c344d33f4db56c44d
darioulin <dariomuse95@gmail.com>
2018-10-25 12:57:23 -0500

    Saving quotation in state

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,21 +1,40 @@
 import React, { Component } from "react";
 import Header from "./Header.jsx";
 import Formulario from "./Formulario.jsx";
-// import { obtenerDiferenciaAnio } from "../helper";
+import { obtenerDiferenciaAnio, calcularMarca, obtenerPlan } from "../helper";
 
 class App extends Component {
+  state = { resultado: "", datos: {} };
+
   cotizarSeguro = datos => {
-    console.log(datos);
-    // const { marca, plan, year } = datos;
-    // // // Agregar un abase de 2000,
-    // let resultado = 2000;
-    //
-    // //Obtener la diferencia de años y por cada año restar el 3%
-    // const diferencia = obtenerDiferenciaAnio(year);
-    // console.log(diferencia);
+    const { marca, plan, year } = datos;
+    // // Agregar un abase de 2000,
+    let resultado = 2000;
+
+    //Obtener la diferencia de años y por cada año restar el 3%
+    const diferencia = obtenerDiferenciaAnio(year);
     // Por cada año restar el 3% al valor del cotizarSeguro
+    resultado -= (diferencia * 3 * resultado) / 100;
 
     // Americano 15% Asiatico y Europeo 30% de incremento al valor actual
+    resultado = calcularMarca(marca) * resultado;
+
+    // el plan del auto, el básico incrementa el valor 20% y cobertura completa 50%
+    let incrementoPlan = obtenerPlan(plan);
+
+    // Dependiendo del plan incrementar
+    resultado = parseFloat(incrementoPlan * resultado).toFixed(2);
+    // Crear objeto para el resumen
+    const datosAuto = {
+      marca: marca,
+      plan: plan,
+      year: year
+    };
+    this.setState({
+      resultado: resultado,
+      datos: datosAuto
+    });
+    console.log(resultado);
   };
   render() {
     return (
